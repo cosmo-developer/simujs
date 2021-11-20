fa695cd47f8fdb29482721611be28e58380fa89e
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-11-20 22:29:59 +0530

    update

--- libs/scrips/simujs.js
+++ libs/scrips/simujs.js
@@ -1,10 +1,12 @@
+//Chart.defaults.datasets.line.showLine = true;
 let chart=new Chart("plotter", {
   type: "scatter",
   data: {
     datasets: [
       {
+        label:["Unfiro"],
         pointRadius: 1,
-        pointBackgroundColor: "rgba(0,0,255,1)",
+        pointBackgroundColor: "rgba(214, 32, 190,1)",
         data: [],
       },
     ],
@@ -12,11 +14,14 @@ let chart=new Chart("plotter", {
   options: {},
 });
 
+
+
 function evalchart(){
     xyValues=[]
     let equ=document.getElementsByClassName("equation").item(0)
-    for (var t=0;t<=1;t+=0.001){
+    for (var t=-1;t<=1;t+=0.001){
         xyValues.push({x:t,y:eval(equ.value)})
     }
     chart.data.datasets[0].data=xyValues
+    chart.update();
 }
